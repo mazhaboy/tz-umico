dc84b33b5a4ce0e4a5f1f3199a278eb7bae53780
Temirlan <68460327+mazhaboy@users.noreply.github.com>
2022-05-21 20:22:50 +0600

    test(main): create structures to iterate

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
+var (
+	bagPath       = "resources/bug/bug.txt"
+	landscapePath = "resources/test-keys/landscape.txt"
+)
+
+func main() {
+	bug, bugColumns, bugRows := Open(bagPath)
+	landscape, landscapeColumns, landscapeRows := Open(landscapePath)
+	fmt.Println(bug, bugColumns, bugRows)
+	fmt.Println(landscape, landscapeColumns, landscapeRows)
+}
+
+func Open(filePath string) ([][]byte, int, int) {
+	bug := make([][]byte, 0)
+	columns := 0
+	rows := 0
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		if columns < len(scanner.Text()) {
+			columns = len(scanner.Text())
+		}
+		bug = append(bug, []byte(scanner.Text()))
+	}
+	err = scanner.Err()
+	if err != nil {
+		log.Fatal()
+	}
+	rows = len(bug)
+	return bug, rows, columns
 }
